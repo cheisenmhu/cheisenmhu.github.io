4314038999675b60cfb8ea842020b6096baba53e
Chris Heise <crheise@icloud.com>
2023-10-16 18:46:42 -0600

    clean code and comments

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/*
+    Programmer: Chris Heise (crheise@icloud.com)
+    Course: BSSD 4350 - Agile Methodologies
+    Instructor: Jonathan Lee
+    Program: Second Page POC
+    Purpose: Test embedding a Hugging Face space in a GitHub hosted page.
+*/
+
+// TODO: Add event listener for resize of window to resize iframe
 
 var w = window.innerWidth;
 var h = window.innerHeight;
